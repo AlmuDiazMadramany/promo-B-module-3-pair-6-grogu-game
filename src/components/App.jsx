c2982fb37f6d366c01d4eefb56465f6afeafa610
MelsCrest <melisacresta@gmail.com>
2025-01-24 11:28:47 +0100

    componentizando y creando funcionalidad al dado

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,65 @@
-//import { useState } from "react"
+import { useState } from "react"
 import "../styles/App.scss"
+import Board from "./Board";
+import Header from "./Header";
 
 function App() {
 
-  /*const [grogu, setGrogu] = useState(0);
+  const [grogu, setGrogu] = useState(0);
   const [cookie, setCookie] = useState(["🍪", "🍪", "🍪"]);
   const [egg, setEgg] = useState(["🥚", "🥚", "🥚"]);
   const [frog, setFrog] = useState(["🐸", "🐸", "🐸"]);
-  const [gameStatus, setGameStatus] = useState("En curso");*/
+  const [gameStatus, setGameStatus] = useState("");
 
-  const getRandomNumber = (max) => {
+  const getRandomNumber = max => 
     Math.ceil(Math.random() * max);
-  }
-  const handleClick = () => {
-    const randomNumber = getRandomNumber(4);
 
-    console.log(randomNumber)
-    /*generar num aleatorio
+  /*generar num aleatorio
     con el num aleatorio, condicional
     */
-  }
+  function rollDice(){
+    const randomNumber = getRandomNumber(4);
+    console.log(randomNumber);
+    if(randomNumber === 4){
+      //Grogu avanza una casilla
+      setGrogu(grogu+1);
+      //Mensaje [gameStatus]: Grogu avanza una casilla
+      setGameStatus('Grogu avanza una casilla');
+      
+    }else if(randomNumber === 3){
+      //Se quita una galleta
+      setCookie(cookie.slice(1));
+      //Mensaje [gameStatus]: [Nombre usuario] has ayudado a Mando a recoger [una galleta]
+      setGameStatus('[Nombre usuario] has ayudado a Mando a recoger [una galleta]');
+      
+    }else if(randomNumber === 2){
+      //Se quita un huevo
+      setEgg(egg.slice(1));
+      //Mensaje [gameStatus]: [Nombre usuario] has ayudado a Mando a recoger [un huevo]
+      setGameStatus('[Nombre usuario] has ayudado a Mando a recoger [un huevo]');
+    }else{
+      //Se quita una rana
+      setFrog(frog.slice(1));
+      //Mensaje [gameStatus]: [Nombre usuario] has ayudado a Mando a recoger [una rana]
+      setGameStatus('[Nombre usuario] has ayudado a Mando a recoger [una rana]');
+    };
+  };
+  
+  const handleClick = () => {
+    rollDice();
+  };
 
   return (
     <>
-     <header>
-      <h1>¡Cuidado con Grogu!</h1>
-    </header>
+    <Header/>
     <main className="page">
-      <section className="board">
-        <div className="cell"><div className="grogu">👣</div></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-      </section>
+      <Board/>
 
       <section>
         <button className="dice" onClick={handleClick}>Lanzar Dado</button>
-        <div className="game-status">En curso</div>
+        <div className="game-status">{gameStatus || 'En curso'}</div>
       </section>
-
+      
       <section className="goods-container">
         <div className="goods-item">🍪</div>
         <div className="goods-item">🍪</div>
